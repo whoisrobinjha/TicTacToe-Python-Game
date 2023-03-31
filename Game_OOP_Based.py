6524dae90ea355b9564ca948d7d82526dbb92fd5
whoisrobinjha <robinjha370@gmail.com>
2023-03-31 10:34:25 +0530

    Game modified

--- Game_OOP_Based.py
+++ Game_OOP_Based.py
@@ -1,10 +1,15 @@
 class TicTacToe:
+    player_X = "X"
+    player_O = "O"
+    empty_space = " "
+
     def __init__(self):
-        self.players = ["X", "O"]
+        self.players = [self.player_X, self.player_O]
         self.current_player = self.players[0]
-        self.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+        self.board = [self.empty_space] * 9
 
     def print_board(self):
+        """print the current state of the board"""
         print(self.board[0] + " | " + self.board[1] + " | " + self.board[2])
         print("- - - - -")
         print(self.board[3] + " | " + self.board[4] + " | " + self.board[5])
@@ -12,28 +17,37 @@ class TicTacToe:
         print(self.board[6] + " | " + self.board[7] + " | " + self.board[8])
 
     def check_winner(self):
+        """Check if the current player has won"""
         winning_position = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
         for position in winning_position:
             if all(self.board[i] == self.current_player for i in position):
                 return True
         return False
+    
+    def get_move(self):
+        """Get user's move"""
+        while True:
+            move = input(self.current_player + "'s turn... Enter a position (1-9): ")
+            if move.isdigit():
+                move = int(move) - 1
+                if 0 <= move and self.board[move] == self.empty_space:
+                    return move
+            print("Invalid move, please try again.")
 
     def play_game(self):
+        """Play the game"""
         print("Welcome to Tic Tac Toe!")
         self.print_board()
         while True:
-            move = int(input(self.current_player + "'s turn... Enter a position (1-9): ")) - 1
-            if self.board[move] != " ":
-                print("Space occupied, try again...")
-            else:
-                self.board[move] = self.current_player
-                self.print_board()
-                if self.check_winner():
-                    print(self.current_player + " Wins!!")
-                    break
-                if all(i != " " for i in self.board):
+            move = self.get_move()
+            self.board[move] = self.current_player
+            self.print_board()
+            if self.check_winner():
+                print(self.current_player + " Wins!!")
+                break
+            if all(i != self.empty_space for i in self.board):
                     print("It is a Draw!!")
-                    break
-                self.current_player = self.players[(self.players.index(self.current_player) + 1) % 2]
+                    break              
+            self.current_player = self.players[(self.players.index(self.current_player) + 1) % 2]
 
 TicTacToe().play_game()
